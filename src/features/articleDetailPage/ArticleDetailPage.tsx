9f402f3ec4147b91c1c44416965d477a0cc4d585
TECHMISTRIZ-05\admin <suraj@techmistriz.com>
2026-02-19 20:42:13 +0530

    responsiveness fix of testimonial

--- src/features/articleDetailPage/ArticleDetailPage.tsx
+++ src/features/articleDetailPage/ArticleDetailPage.tsx
@@ -189,14 +189,13 @@ export default function ArticleDetailPage() {
     }
   };
 
-const categoryTitle = toTitleCase(
-  article.category
-    ? typeof article.category === "string"
-      ? article.category
-      : article.category.slug
-    : category || ""
-);
-
+  const categoryTitle = toTitleCase(
+    article.category
+      ? typeof article.category === "string"
+        ? article.category
+        : article.category.slug
+      : category || "",
+  );
 
   console.log("Singlepage", article);
 
@@ -326,14 +325,17 @@ const categoryTitle = toTitleCase(
               <h3 className="font-bold text-xl mt-10">ABOUT AUTHOR</h3>
               <div className="w-10 h-1 bg-[#c9060a]" />
               <div className="border border-gray-300 mt-2 p-4 flex gap-4   transition-none hover:shadow-[0_-6px_15px_rgba(0,0,0,0.15),0_6px_15px_rgba(0,0,0,0.15)]">
-               <div className="relative w-24 h-24 overflow-hidden bg-gray-200 shrink-0 ">
-  <Image
-    src={getAuthorAvatar(article.author?.avatar, article.author?.name)}
-    alt={article.author?.name || "Author"}
-    fill
-    className="object-cover"
-  />
-</div>
+                <div className="relative w-24 h-24 overflow-hidden bg-gray-200 shrink-0 ">
+                  <Image
+                    src={getAuthorAvatar(
+                      article.author?.avatar,
+                      article.author?.name,
+                    )}
+                    alt={article.author?.name || "Author"}
+                    fill
+                    className="object-cover"
+                  />
+                </div>
 
                 <div className="flex-1 min-w-0 ">
                   <h4 className="font-semibold text-base mb-2">
