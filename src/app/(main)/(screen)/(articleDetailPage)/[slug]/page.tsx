3be6a431d25c4d33f4f250837b01a18756d88ab7
TECHMISTRIZ-05\admin <suraj@techmistriz.com>
2026-02-23 12:36:30 +0530

    fixed errors

--- src/app/(main)/(screen)/(articleDetailPage)/[slug]/page.tsx
+++ src/app/(main)/(screen)/(articleDetailPage)/[slug]/page.tsx
@@ -1,36 +1,10 @@
-import ArticleDetailPage from "@/features/articleDetailPage/ArticleDetailPage";
-import { getArticleBySlug } from "@/lib/api/services/posts";
+import ArticleDetailPage from '@/features/articleDetailPage/ArticleDetailPage'
+import React from 'react'
 
-export async function generateMetadata({ params }: any) {
-  const article = await getArticleBySlug(params.slug);
-
-  if (!article) {
-    return { title: "Article" };
-  }
-
-  const imageUrl = article.image?.startsWith("http")
-    ? article.image
-    : `https://lexwitness.com/${article.image}`;
-
-  return {
-    title: article.title,
-    description: article.description?.slice(0, 160),
-    openGraph: {
-      title: article.title,
-      description: article.description?.slice(0, 160),
-      url: `https://lexwitness.com/${params.slug}`,
-      images: [
-        {
-          url: imageUrl,
-          width: 1200,
-          height: 630,
-        },
-      ],
-      type: "article",
-    },
-  };
+function page() {
+  return (
+    <ArticleDetailPage/>
+  )
 }
 
-export default function Page({ params }: any) {
-  return <ArticleDetailPage slug={params.slug} />;
-}
+export default page
