9f7e717d1b945c1a33a1e6cac1512c9e5ab4c5df
TECHMISTRIZ-05\admin <suraj@techmistriz.com>
2026-03-12 12:58:01 +0530

    social icons hover effect change

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -55,7 +55,7 @@ const Footer = () => {
           <path d="M4.98 3.5C4.98 4.88 3.88 6 2.5 6S0 4.88 0 3.5 1.12 1 2.5 1 4.98 2.12 4.98 3.5zM0 8h5v16H0V8zm7.5 0h4.78v2.22h.07c.66-1.25 2.27-2.57 4.68-2.57 5 0 5.92 3.28 5.92 7.55V24h-5v-7.92c0-1.89-.03-4.33-2.63-4.33-2.63 0-3.03 2.05-3.03 4.17V24h-5V8z" />
         </svg>
 
-        <span className="absolute inset-0 bg-black opacity-0 group-hover:opacity-10 transition-opacity duration-300 pointer-events-none"></span>
+        <span className="absolute inset-0 bg-black opacity-0 group-hover:opacity-10   pointer-events-none"></span>
       </a>
                   </div>
 
