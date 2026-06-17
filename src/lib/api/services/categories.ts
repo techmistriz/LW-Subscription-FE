457957666f49927cd0b202a944a08b3d4414613a
TECHMISTRIZ-05\admin <suraj@techmistriz.com>
2026-06-17 15:44:35 +0530

    no message

--- src/lib/api/services/categories.ts
+++ src/lib/api/services/categories.ts
@@ -2,12 +2,9 @@ import { request } from "@/lib/api/request";
 import { Category } from "@/types";
 
 export async function getCategories(): Promise<Category[]> {
-  const response = await request<any>(
-    "GET",
-    "/categories?is_show_in_menu=1"
-  );
+  const response = await request<any>("GET", "/categories?is_show_in_menu=1");
 
-  console.log("CATEGORIES RESPONSE:", response);
+  // console.log("CATEGORIES RESPONSE:", response);
 
   if (response?.data?.status === false) {
     console.error("❌ Categories API Error");
