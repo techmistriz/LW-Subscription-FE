a59d7a4422196b12137dcf067d8daf5bdacd7e16
TECHMISTRIZ-05\admin <suraj@techmistriz.com>
2026-09-04 17:16:20 +0530

    ...

--- src/features/SubscriptionPage.tsx
+++ src/features/SubscriptionPage.tsx
@@ -172,7 +172,7 @@ export default function SubscriptionPage() {
           </p>
 
           <div className="mt-6">
-            <h1 className="text-xl lg:text-2xl  font-bold text-[#333] leading-tight">
+            <h1 className="text-xl lg:text-2xl  font-bold text-[#333] leading-snug">
               Your <span className="text-[#c9060a]">1st Month</span> is on Us.
             </h1>
 
