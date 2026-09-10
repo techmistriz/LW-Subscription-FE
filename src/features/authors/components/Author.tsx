4d33b6d3da98a1784cd8c7a6fb324eeb12cfcacd
TECHMISTRIZ-05\admin <suraj@techmistriz.com>
2026-09-10 23:41:35 +0530

    hide author linked

--- src/features/authors/components/Author.tsx
+++ src/features/authors/components/Author.tsx
@@ -47,14 +47,14 @@ function Author({ data }: AuthorProps) {
             <p className="text-sm text-[#333] mt-1">{data.designation}</p>
 
             <p className="text-sm text-[#c9060a] mt-1 font-medium">
-              {data.company_name}
-            </p>
+                {data.company_name}
+              </p>
 
             <p className="text-sm text-gray-400 mt-1 font-medium">
               {data.place}
             </p>
 
-            {data.linkedin && (
+            {/* {data.linkedin && (
               <a
                 href={data.linkedin}
                 target="_blank"
@@ -69,7 +69,7 @@ function Author({ data }: AuthorProps) {
                   <path d="M4.98 3.5C4.98 4.88 3.88 6 2.5 6S0 4.88 0 3.5 1.12 1 2.5 1 4.98 2.12 4.98 3.5zM0 8h5v16H0V8zm7.5 0h4.78v2.22h.07c.66-1.25 2.27-2.57 4.68-2.57 5 0 5.92 3.28 5.92 7.55V24h-5v-7.92c0-1.89-.03-4.33-2.63-4.33-2.63 0-3.03 2.05-3.03 4.17V24h-5V8z" />
                 </svg>
               </a>
-            )}
+            )} */}
           </div>
         </div>
 
